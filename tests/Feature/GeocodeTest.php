fa40258248f5e53497ae6b0af9a97908e0156769
Mior Muhammad Zaki <crynobone@gmail.com>
2019-08-04 15:58:10 +0800

    Add tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/GeocodeTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Katsana\Geocoder\Tests\Feature\Geocoder;
+
+use Mockery as m;
+use Geocoder\Geocoder;
+use Katsana\Geocoder\Location;
+use Orchestra\Testbench\TestCase;
+use Illuminate\Contracts\Cache\Repository;
+
+class GeocodeTest extends TestCase
+{
+    /** @test */
+    public function it_doesnt_cache_proximity_if_location_is_null()
+    {
+        $geocode = new Geocode(3.1339905, 101.6070148);
+        $location = null;
+
+        $cache = m::mock(Repository::class);
+
+        $cache->shouldReceive('has')->never()
+                ->shouldReceive('put')->never();
+
+        $geocode->cache($cache, $location);
+    }
+
+    /** @test */
+    public function it_doesnt_cache_proximity_if_already_stored()
+    {
+        $data = [
+            'coordinate' => '3.1339905,101.6070148',
+            'latitude' => 3.1339905,
+            'longitude' => 101.6070148,
+            'street_number' => null,
+            'street_name' => 'Lebuh Bandar Utama',
+            'locality' => 'Petaling Jaya',
+            'sublocality' => 'Bandar Utama',
+            'postcode' => '47800',
+            'country' => 'Malaysia',
+            'address' => 'Lebuh Bandar Utama, 47800 Bandar Utama, Petaling Jaya, Malaysia',
+        ];
+
+        $geocode = new Geocode(3.1339905, 101.6070148);
+        $location = Location::parse('3.1339905,101.6070148', $data);
+
+        $cache = m::mock(Repository::class);
+
+        $cache->shouldReceive('has')->once()
+                    ->with("geocoder.proximity.50:{$geocode->getProximityCoordinate()}")->andReturn(true)
+                ->shouldReceive('put')->never();
+
+        $geocode->cache($cache, $location);
+    }
+
+    /** @test */
+    public function it_can_force_cache_proximity_if_already_stored()
+    {
+        $data = [
+            'coordinate' => '3.1339905,101.6070148',
+            'latitude' => 3.1339905,
+            'longitude' => 101.6070148,
+            'street_number' => null,
+            'street_name' => 'Lebuh Bandar Utama',
+            'locality' => 'Petaling Jaya',
+            'sublocality' => 'Bandar Utama',
+            'postcode' => '47800',
+            'country' => 'Malaysia',
+            'address' => 'Lebuh Bandar Utama, 47800 Bandar Utama, Petaling Jaya, Malaysia',
+        ];
+
+        $geocode = new Geocode(3.1339905, 101.6070148);
+        $location = Location::parse('3.1339905,101.6070148', $data);
+        $cacheKey = "geocoder.proximity.50:{$geocode->getProximityCoordinate()}";
+
+        $cache = m::mock(Repository::class);
+
+        $cache->shouldReceive('has')->once()
+                    ->with($cacheKey)->andReturn(true)
+                ->shouldReceive('put')->once()
+                    ->with($cacheKey, $location->toArray(), 2592000);
+
+        $geocode->cache($cache, $location, true);
+    }
+}
+
+class Geocode extends \Katsana\Geocoder\Geocode
+{
+    /**
+     * Fetch from local provider.
+     *
+     * @param \Geocoder\Geocoder                     $geocoder
+     * @param \Illuminate\Contracts\Cache\Repository $cache
+     *
+     * @return \Katsana\Geocoder\Location|null
+     */
+    public function queryFromLocalProvider(Geocoder $geocoder, Repository $cache): ?Location
+    {
+        return null;
+    }
+}
